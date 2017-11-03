e2396702454270cff1a8a909ad6a542d0e560b8d
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-11-03 18:06:16 +0200

    Added solution for Pr.06

---
+++ 04. OOP Intro Exercises/06. Online Radio Database/Song.cs
@@ -0,0 +1,98 @@
+﻿public class Song
+{
+    private const int MinLengthName = 3;
+    private const int MaxLengthArtistName = 20;
+    private const int MaxLengthSongName = 30;
+    private const int MinimumLength = 0;
+    private const int MaxMinutes = 14;
+    private const int MaxSeconds = 59;
+
+    private string artistName;
+    private string songName;
+    private int minutes;
+    private int seconds;
+
+    public Song(string artist, string name, int minutes, int seconds)
+    {
+        this.ArtistName = artist;
+        this.SongName = name;
+        this.Minutes = minutes;
+        this.Seconds = seconds;
+    }
+
+    public string ArtistName
+    {
+        get
+        {
+            return this.artistName;
+        }
+
+        private set
+        {
+            if (string.IsNullOrWhiteSpace(value) ||         // razlika 2
+                value.Length < MinLengthName ||
+                value.Length > MaxLengthArtistName)
+            {
+                throw new InvalidArtistNameException();
+            }
+
+            this.artistName = value;
+        }
+    }
+
+    public string SongName
+    {
+        get
+        {
+            return this.songName;
+        }
+
+        private set
+        {
+            if (string.IsNullOrWhiteSpace(value) ||     // razlika 1
+                value.Length < MinLengthName ||
+                value.Length > MaxLengthSongName)
+            {
+                throw new InvalidSongNameException();
+            }
+
+            this.songName = value;
+        }
+    }
+
+    public int Minutes
+    {
+        get
+        {
+            return this.minutes;
+        }
+
+        private set
+        {
+            if (value < MinimumLength || value > MaxMinutes)
+            {
+                throw new InvalidSongMinutesException();
+            }
+
+            this.minutes = value;
+        }
+    }
+
+    public int Seconds
+    {
+        get
+        {
+            return this.seconds;
+        }
+
+        private set
+        {
+            if (value < MinimumLength || value > MaxSeconds)
+            {
+                throw new InvalidSongSecondsException();
+            }
+
+            this.seconds = value;
+        }
+    }
+}
---
+++ 04. OOP Intro Exercises/06. Online Radio Database/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var playlist = new List<Song>();
+
+        int loopEnd = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < loopEnd; i++)
+        {
+            string[] input = Console.ReadLine().Split(';');
+
+            try
+            {
+
+                string artist = input[0];
+                string songName = input[1];
+                string[] songLength = input[2].Split(':');
+                int minutes = int.Parse(songLength[0]);
+                int seconds = int.Parse(songLength[1]);
+
+                var song = new Song(artist, songName, minutes, seconds);
+
+                playlist.Add(song);
+
+                Console.WriteLine("Song added.");
+            }
+            catch (InvalidSongException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine(new InvalidSongLengthException().Message);
+            }
+        }
+
+        var totalTime = playlist.Sum(t => t.Minutes * 60) + playlist.Sum(s => s.Seconds);
+        long listSeconds = totalTime % 60;
+        long listMinutes = (totalTime / 60) % 60;
+        long listHours = (totalTime / 60 / 60) % 24;
+
+        Console.WriteLine($"Songs added: {playlist.Count}");
+        Console.WriteLine($"Playlist length: {listHours}h {listMinutes}m {listSeconds}s");
+    }
+}
