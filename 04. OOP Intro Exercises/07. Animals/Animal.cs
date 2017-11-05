c9a5cb62bb771e1214bca756e202717cda71c0a7
Plamen Stoyanov <pazzo_bg@yahoo.com>
2017-11-05 23:01:36 +0200

    Added solution to Pr.07 - Animals

---
+++ 04. OOP Intro Exercises/07. Animals/Animal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+public class Animal
+{
+    private string name;
+    private int age;
+    private string gender;
+
+    public Animal(string name, int age, string gender)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+
+            this.name = value;      
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public string Gender
+    {
+        get
+        {
+            return this.gender;
+        }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Invalid input!");
+            }
+
+            this.gender = value;
+        }
+    }
+
+    public virtual string ProduceSound()
+    {
+        return string.Empty;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name}" + Environment.NewLine + 
+            $"{this.Name} {this.Age} {this.Gender}" + Environment.NewLine + 
+            $"{this.ProduceSound()}";
+    }
+}
---
+++ 04. OOP Intro Exercises/07. Animals/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var population = new List<Animal>();
+
+        string animal = Console.ReadLine();
+
+        while (animal != "Beast!")
+        {
+            string[] tokens = Console.ReadLine().Split();
+            string name = tokens[0];
+            int age = int.Parse(tokens[1]);
+            string gender = tokens[2];
+
+            try
+            {
+                switch (animal)
+                {
+                    case "Dog": population.Add(new Dog(name, age, gender)); break;
+                    case "Cat": population.Add(new Cat(name, age, gender)); break;
+                    case "Frog": population.Add(new Frog(name, age, gender)); break;
+                    case "Kitten": population.Add(new Kitten(name, age, gender)); break;
+                    case "Tomcat": population.Add(new Tomcat(name, age, gender)); break;
+                    default: throw new ArgumentException("Invalid input!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            animal = Console.ReadLine();
+        }
+
+        foreach (var creature in population)
+        {
+            Console.WriteLine(creature);
+        }
+    }
+}
